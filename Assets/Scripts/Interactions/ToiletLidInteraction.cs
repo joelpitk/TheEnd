63d48cee02dbebe3f28ee5c9354eae4b7e885d15
Joel Pitkänen <joel.pitkanen@cs.helsinki.fi>
2014-10-09 22:33:52 +0300

    Implemented opening/closing of toilet lid.

---
+++ Assets/Scripts/Interactions/ToiletLidInteraction.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Interactions
+{
+    public class ToiletLidInteraction : Interaction
+    {
+        private bool isOpen;
+        private Animation parentAnimation;
+
+        public void Awake()
+        {
+            parentAnimation = gameObject.transform.parent.animation;
+        }
+
+        public override void Activate(GameObject player, GameObject itemInHand)
+        {
+            if (CanOpen())
+            {
+                Open();
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private bool CanOpen()
+        {
+            return !isOpen && !parentAnimation.isPlaying;
+        }
+
+        private void Open()
+        {
+            parentAnimation.Play("OpenKansi");
+            isOpen = true;
+        }
+
+        private void Close()
+        {
+            parentAnimation.Play("CloseKansi");
+            isOpen = false;
+        }
+    }
+}
