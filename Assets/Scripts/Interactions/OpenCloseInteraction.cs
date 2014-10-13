cbdd33bed68bf4a4057545fe452920b1979b7a22
Joel Pitkänen <joel.pitkanen@cs.helsinki.fi>
2014-10-13 22:25:33 +0300

    Added jaakaappi.blend. Created jaakaappi-prefab.

---
+++ Assets/Scripts/Interactions/OpenCloseInteraction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Interactions
+{
+    public class OpenCloseInteraction : Interaction
+    {
+        private bool isOpen;
+
+        public override void Activate(GameObject player, GameObject itemInHand)
+        {
+            if (isOpen)
+            {
+                transform.parent.animation.Play("Close");
+                isOpen = false;
+            }
+            else
+            {
+                transform.parent.animation.Play("Open");
+                isOpen = true;
+            }
+        }
+    }
+}
