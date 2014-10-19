a62be6225503fa65518a47de24bbb0345bffecee
Joel Pitkänen <joel.pitkanen@cs.helsinki.fi>
2014-10-19 22:09:20 +0300

    Opening/Closing uuni.

--- Assets/Scripts/Interactions/OpenCloseInteraction.cs
+++ Assets/Scripts/Interactions/OpenCloseInteraction.cs
@@ -5,17 +5,19 @@ namespace Assets.Scripts.Interactions
     public class OpenCloseInteraction : Interaction
     {
         private bool isOpen;
+        public string closingAnimationName = "Close";
+        public string openingAnimationName = "Open";
 
         public override void Activate(GameObject player, GameObject itemInHand)
         {
             if (isOpen)
             {
-                transform.parent.animation.Play("Close");
+                transform.parent.animation.Play(closingAnimationName);
                 isOpen = false;
             }
             else
             {
-                transform.parent.animation.Play("Open");
+                transform.parent.animation.Play(openingAnimationName);
                 isOpen = true;
             }
         }
